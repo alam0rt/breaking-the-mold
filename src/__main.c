3c57210470396ea2e8adfb02c17e70ebd88aaf1a
alam0rt <sam@samlockart.com>
2026-01-01 16:22:25 +1100

    compiles

--- src/__main.c
+++ src/__main.c
@@ -1,9 +1,5 @@
 #include "common.h"
 
-// INCLUDE_ASM("asm/pal/nonmatchings/__main", __main);
-
-void __main(void) {
-    return;
-}
+INCLUDE_ASM("asm/pal/nonmatchings/__main", __main);
 
 INCLUDE_ASM("asm/pal/nonmatchings/__main", func_8008E750);
