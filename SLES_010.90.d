8be84be5235ba66dd35eaf161df81e5f7646b36d
alam0rt <sam@samlockart.com>
2026-01-22 20:05:42 +1100

    linker

---
+++ SLES_010.90.d
@@ -0,0 +1,123 @@
+build/SLES_010.90.elf: \
+    build/asm/header.o \
+    build/asm/data/800.rodata.o \
+    build/asm/data/E2C.rodata.o \
+    build/asm/data/1F28.rodata.o \
+    build/asm/data/2044.rodata.o \
+    build/asm/data/LIBCD.rodata.o \
+    build/asm/data/LIBGPU.rodata.o \
+    build/asm/data/LIBSPU.rodata.o \
+    build/asm/Game/RENDER.o \
+    build/asm/Game/ENTITY.o \
+    build/asm/Game/OBJECT.o \
+    build/asm/Game/PLAYER.o \
+    build/asm/Game/GAMELOOP.o \
+    build/asm/LIBS.o \
+    build/asm/data/800.rodata.o \
+    build/asm/data/E2C.rodata.o \
+    build/asm/data/1F28.rodata.o \
+    build/asm/data/2044.rodata.o \
+    build/asm/data/LIBCD.rodata.o \
+    build/asm/data/LIBGPU.rodata.o \
+    build/asm/data/LIBSPU.rodata.o \
+    build/asm/Game/RENDER.o \
+    build/asm/Game/ENTITY.o \
+    build/asm/Game/OBJECT.o \
+    build/asm/Game/PLAYER.o \
+    build/asm/Game/GAMELOOP.o \
+    build/asm/LIBS.o \
+    build/asm/data/800.rodata.o \
+    build/asm/data/E2C.rodata.o \
+    build/asm/data/1F28.rodata.o \
+    build/asm/data/2044.rodata.o \
+    build/asm/data/LIBCD.rodata.o \
+    build/asm/data/LIBGPU.rodata.o \
+    build/asm/data/LIBSPU.rodata.o \
+    build/asm/Game/RENDER.o \
+    build/asm/Game/ENTITY.o \
+    build/asm/Game/OBJECT.o \
+    build/asm/Game/PLAYER.o \
+    build/asm/Game/GAMELOOP.o \
+    build/asm/LIBS.o \
+    build/asm/Game/RENDER.o \
+    build/asm/Game/ENTITY.o \
+    build/asm/Game/OBJECT.o \
+    build/asm/Game/PLAYER.o \
+    build/asm/Game/GAMELOOP.o \
+    build/asm/LIBS.o \
+    build/asm/data/800.rodata.o \
+    build/asm/data/E2C.rodata.o \
+    build/asm/data/1F28.rodata.o \
+    build/asm/data/2044.rodata.o \
+    build/asm/data/LIBCD.rodata.o \
+    build/asm/data/LIBGPU.rodata.o \
+    build/asm/data/LIBSPU.rodata.o \
+    build/asm/Game/RENDER.o \
+    build/asm/Game/ENTITY.o \
+    build/asm/Game/OBJECT.o \
+    build/asm/Game/PLAYER.o \
+    build/asm/Game/GAMELOOP.o \
+    build/asm/LIBS.o \
+    build/asm/data/80FEC.data.o \
+    build/asm/data/96154.sdata.o
+build/asm/header.o:
+build/asm/data/800.rodata.o:
+build/asm/data/E2C.rodata.o:
+build/asm/data/1F28.rodata.o:
+build/asm/data/2044.rodata.o:
+build/asm/data/LIBCD.rodata.o:
+build/asm/data/LIBGPU.rodata.o:
+build/asm/data/LIBSPU.rodata.o:
+build/asm/Game/RENDER.o:
+build/asm/Game/ENTITY.o:
+build/asm/Game/OBJECT.o:
+build/asm/Game/PLAYER.o:
+build/asm/Game/GAMELOOP.o:
+build/asm/LIBS.o:
+build/asm/data/800.rodata.o:
+build/asm/data/E2C.rodata.o:
+build/asm/data/1F28.rodata.o:
+build/asm/data/2044.rodata.o:
+build/asm/data/LIBCD.rodata.o:
+build/asm/data/LIBGPU.rodata.o:
+build/asm/data/LIBSPU.rodata.o:
+build/asm/Game/RENDER.o:
+build/asm/Game/ENTITY.o:
+build/asm/Game/OBJECT.o:
+build/asm/Game/PLAYER.o:
+build/asm/Game/GAMELOOP.o:
+build/asm/LIBS.o:
+build/asm/data/800.rodata.o:
+build/asm/data/E2C.rodata.o:
+build/asm/data/1F28.rodata.o:
+build/asm/data/2044.rodata.o:
+build/asm/data/LIBCD.rodata.o:
+build/asm/data/LIBGPU.rodata.o:
+build/asm/data/LIBSPU.rodata.o:
+build/asm/Game/RENDER.o:
+build/asm/Game/ENTITY.o:
+build/asm/Game/OBJECT.o:
+build/asm/Game/PLAYER.o:
+build/asm/Game/GAMELOOP.o:
+build/asm/LIBS.o:
+build/asm/Game/RENDER.o:
+build/asm/Game/ENTITY.o:
+build/asm/Game/OBJECT.o:
+build/asm/Game/PLAYER.o:
+build/asm/Game/GAMELOOP.o:
+build/asm/LIBS.o:
+build/asm/data/800.rodata.o:
+build/asm/data/E2C.rodata.o:
+build/asm/data/1F28.rodata.o:
+build/asm/data/2044.rodata.o:
+build/asm/data/LIBCD.rodata.o:
+build/asm/data/LIBGPU.rodata.o:
+build/asm/data/LIBSPU.rodata.o:
+build/asm/Game/RENDER.o:
+build/asm/Game/ENTITY.o:
+build/asm/Game/OBJECT.o:
+build/asm/Game/PLAYER.o:
+build/asm/Game/GAMELOOP.o:
+build/asm/LIBS.o:
+build/asm/data/80FEC.data.o:
+build/asm/data/96154.sdata.o:
