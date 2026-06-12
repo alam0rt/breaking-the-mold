6d5ddecdc1a8d0e90f1ad798bd1e504f7bebb2e4
alam0rt <sam@samlockart.com>
2026-06-13 09:49:28 +1000

    Split rodata at jumptable alignment boundaries

--- SLES_010.90.d
+++ SLES_010.90.d
@@ -4,7 +4,9 @@ build/SLES_010.90.elf: \
     build/src/system/early_stub.o \
     build/asm/Game/INIT_TABLES.o \
     build/asm/data/Game/OBJECT.rodata.o \
+    build/asm/data/Game/PLAYER_EARLY.rodata.o \
     build/asm/data/Game/PLAYER.rodata.o \
+    build/asm/data/Game/GAMELOOP_EARLY.rodata.o \
     build/asm/data/Game/GAMELOOP.rodata.o \
     build/asm/data/assets/blb_memory.rodata.o \
     build/asm/data/LIBCD.rodata.o \
@@ -24,7 +26,9 @@ build/SLES_010.90.elf: \
     build/src/libs/libspu_voice.o \
     build/asm/LIBS_80FD0.o \
     build/asm/Game/OBJECT.o \
+    build/asm/Game/PLAYER_EARLY.o \
     build/asm/Game/PLAYER.o \
+    build/asm/Game/GAMELOOP_EARLY.o \
     build/asm/Game/GAMELOOP.o \
     build/src/assets/blb_memory.o \
     build/asm/LIBCD.o \
@@ -39,7 +43,9 @@ build/asm/Game/INIT.o:
 build/src/system/early_stub.o:
 build/asm/Game/INIT_TABLES.o:
 build/asm/data/Game/OBJECT.rodata.o:
+build/asm/data/Game/PLAYER_EARLY.rodata.o:
 build/asm/data/Game/PLAYER.rodata.o:
+build/asm/data/Game/GAMELOOP_EARLY.rodata.o:
 build/asm/data/Game/GAMELOOP.rodata.o:
 build/asm/data/assets/blb_memory.rodata.o:
 build/asm/data/LIBCD.rodata.o:
@@ -59,7 +65,9 @@ build/src/libs/memmove.o:
 build/src/libs/libspu_voice.o:
 build/asm/LIBS_80FD0.o:
 build/asm/Game/OBJECT.o:
+build/asm/Game/PLAYER_EARLY.o:
 build/asm/Game/PLAYER.o:
+build/asm/Game/GAMELOOP_EARLY.o:
 build/asm/Game/GAMELOOP.o:
 build/src/assets/blb_memory.o:
 build/asm/LIBCD.o:
@@ -69,4 +77,4 @@ build/asm/data/80FEC.data.o:
 build/asm/data/96154.sdata.o:
 build/asm/data/96920.sbss.o:
 build/asm/data/96920.bss.o:
--include build/asm/header.d build/asm/Game/INIT.d build/src/system/early_stub.d build/asm/Game/INIT_TABLES.d build/asm/data/Game/OBJECT.rodata.d build/asm/data/Game/PLAYER.rodata.d build/asm/data/Game/GAMELOOP.rodata.d build/asm/data/assets/blb_memory.rodata.d build/asm/data/LIBCD.rodata.d build/asm/data/LIBGPU.rodata.d build/asm/data/LIBSPU.rodata.d build/asm/Game/RENDER.d build/src/render/stub_vibrate_off.d build/asm/Game/RENDER_5C3C.d build/src/render/empty_stub_18d4c.d build/asm/Game/RENDER_9554.d build/asm/Game/ENTITY.d build/src/entity/destructor_spu_at10c.d build/src/world/static_game_state.d build/src/system/empty_callbacks.d build/asm/libs/bios_trampolines.d build/src/libs/memmove.d build/src/libs/libspu_voice.d build/asm/LIBS_80FD0.d build/asm/Game/OBJECT.d build/asm/Game/PLAYER.d build/asm/Game/GAMELOOP.d build/src/assets/blb_memory.d build/asm/LIBCD.d build/asm/LIBGPU.d build/asm/LIBSPU.d build/asm/data/80FEC.data.d build/asm/data/96154.sdata.d build/asm/data/96920.sbss.d build/asm/data/96920.bss.d
+-include build/asm/header.d build/asm/Game/INIT.d build/src/system/early_stub.d build/asm/Game/INIT_TABLES.d build/asm/data/Game/OBJECT.rodata.d build/asm/data/Game/PLAYER_EARLY.rodata.d build/asm/data/Game/PLAYER.rodata.d build/asm/data/Game/GAMELOOP_EARLY.rodata.d build/asm/data/Game/GAMELOOP.rodata.d build/asm/data/assets/blb_memory.rodata.d build/asm/data/LIBCD.rodata.d build/asm/data/LIBGPU.rodata.d build/asm/data/LIBSPU.rodata.d build/asm/Game/RENDER.d build/src/render/stub_vibrate_off.d build/asm/Game/RENDER_5C3C.d build/src/render/empty_stub_18d4c.d build/asm/Game/RENDER_9554.d build/asm/Game/ENTITY.d build/src/entity/destructor_spu_at10c.d build/src/world/static_game_state.d build/src/system/empty_callbacks.d build/asm/libs/bios_trampolines.d build/src/libs/memmove.d build/src/libs/libspu_voice.d build/asm/LIBS_80FD0.d build/asm/Game/OBJECT.d build/asm/Game/PLAYER_EARLY.d build/asm/Game/PLAYER.d build/asm/Game/GAMELOOP_EARLY.d build/asm/Game/GAMELOOP.d build/src/assets/blb_memory.d build/asm/LIBCD.d build/asm/LIBGPU.d build/asm/LIBSPU.d build/asm/data/80FEC.data.d build/asm/data/96154.sdata.d build/asm/data/96920.sbss.d build/asm/data/96920.bss.d
