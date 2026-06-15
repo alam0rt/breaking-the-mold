f948f3babb51477af93b371b7aea2859e323e285
alam0rt <sam@samlockart.com>
2026-06-15 18:09:57 +1000

    decompile StartLevelWithResetOrAdvance

--- src/gstate.c
+++ src/gstate.c
@@ -13,5 +13,20 @@ INCLUDE_ASM("asm/nonmatchings/gstate", InitGameState);
 
 INCLUDE_ASM("asm/nonmatchings/gstate", RespawnAfterDeath);
 
-INCLUDE_ASM("asm/nonmatchings/gstate", StartLevelWithResetOrAdvance);
+extern void AdvanceLevelSequence(u8 *p);
+extern void SeekToLevelInSequence(u8 *p, s32 a, s32 b, s32 c);
+extern u8 SetupAndStartLevel(u8 *p, s32 mode);
+extern void initPlayerState(void *p);
+extern void *D_800A597C;
+
+void StartLevelWithResetOrAdvance(u8 *p, u32 mode) {
+    if (mode & 0xFF) {
+        AdvanceLevelSequence(p + 0x84);
+    } else {
+        SeekToLevelInSequence(p + 0x84, 0, 1, 0);
+    }
+    if (SetupAndStartLevel(p, 0x63) == 0) {
+        initPlayerState(D_800A597C);
+    }
+}
 
