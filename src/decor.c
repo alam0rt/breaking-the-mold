785cde12884c514bab091555a4e761a8d01fe484
alam0rt <sam@samlockart.com>
2026-06-15 16:42:43 +1000

    decompile InitPathDecorNoDataWithSprite (sprite + path init)

--- src/decor.c
+++ src/decor.c
@@ -2,6 +2,9 @@
 #include "functions.h"
 
 extern void *g_pBlbHeapBase;
+extern void *D_80010870;
+extern void *InitEntityWithSprite(void *entity, void *spriteDef, s32 z, s16 x, s16 y);
+extern void InitPathFollowingDecorEntity(void *e, u32 flags, u32 mode);
 
 void FreeEntityNoTeardown_8002c7d8(void *e, u32 size) {
     FreeFromHeap(g_pBlbHeapBase, e, 0, 0);
@@ -13,7 +16,12 @@ INCLUDE_ASM("asm/nonmatchings/decor", InitPathDecorEntityWithSprite);
 
 INCLUDE_ASM("asm/nonmatchings/decor", InitPathDecorNoData);
 
-INCLUDE_ASM("asm/nonmatchings/decor", InitPathDecorNoDataWithSprite);
+void *InitPathDecorNoDataWithSprite(void *e, s32 x, s32 y, void *spriteDef) {
+    InitEntityWithSprite(e, spriteDef, 0x3DE, (s16)x, (s16)y);
+    *(void **)((u8 *)e + 0x18) = &D_80010870;
+    InitPathFollowingDecorEntity(e, 0, 1);
+    return e;
+}
 
 INCLUDE_ASM("asm/nonmatchings/decor", InitPathFollowingDecorEntity);
 
