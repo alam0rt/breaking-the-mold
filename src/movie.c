b75dc79189abb8ca8a80a112bf6f62ded8632575
alam0rt <sam@samlockart.com>
2026-06-15 19:35:28 +1000

    decompile SeekAndStartCDRead

--- src/movie.c
+++ src/movie.c
@@ -25,7 +25,18 @@ INCLUDE_ASM("asm/nonmatchings/movie", DecodeStaticFrame);
 
 INCLUDE_ASM("asm/nonmatchings/movie", WaitForDecodeBufferSync);
 
-INCLUDE_ASM("asm/nonmatchings/movie", SeekAndStartCDRead);
+extern s32 CdControl(s32 cmd, void *param, void *result);
+extern s32 CdRead2(s32 mode);
+extern void VSync(s32 mode);
+
+void SeekAndStartCDRead(void *loc) {
+    u8 mode = 0x80;
+    do {
+        while (CdControl(2, loc, 0) == 0);
+        while (CdControl(0xE, &mode, 0) == 0);
+        VSync(3);
+    } while (CdRead2(0x1E0) == 0);
+}
 
 INCLUDE_ASM("asm/nonmatchings/movie", SetMovieDisplayEnvironment);
 
